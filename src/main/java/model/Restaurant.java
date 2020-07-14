2ec6600d29af9fe98d419b48a7f255b4a8b38094
Валентин <moysait1984@yandex.ru>
2020-07-14 22:01:44 +0400

    initDB_postgres.sql fix restaraunt add annotations

--- src/main/java/model/Restaurant.java
+++ src/main/java/model/Restaurant.java
@@ -1,15 +1,35 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "restaraunt")
 public class Restaurant {
 
+    @Id
     private int restaraunt_Id;
 
+    @Column(name = "name_restaraunt")
+    @NotBlank
     private String name_restaraunt;
 
+    @Column(name = "vote_count")
+    @NotNull
     private int vote_count;
 
+    @Column(name = "menu")
+    @NotBlank
     private String menu;
 
     public Restaurant() {
+    }
+
+    public Restaurant(int restaraunt_Id) {
+        this.restaraunt_Id = restaraunt_Id;
     }
 }
