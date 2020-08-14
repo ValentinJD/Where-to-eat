2c4434049d1c343feec04d8096dfad1ea1bb7300
Валентин <moysait1984@yandex.ru>
2020-08-14 21:33:04 +0400

    fix db add index.jsp

--- src/main/resources/db/initDB_postgres.sql
+++ src/main/resources/db/initDB_postgres.sql
@@ -1,68 +1,95 @@
--- ALTER TABLE "history_votes" DROP CONSTRAINT IF EXISTS "history_votes_fk0";
---
--- ALTER TABLE "roles" DROP CONSTRAINT IF EXISTS "roles_fk0";
-
 DROP TABLE IF EXISTS "users";
 
-DROP TABLE IF EXISTS "restaraunt";
+DROP TABLE IF EXISTS "restaraunts";
 
-DROP TABLE IF EXISTS "history_votes";
+DROP TABLE IF EXISTS "history_votes_users";
 
 DROP TABLE IF EXISTS "roles";
 
+DROP TABLE IF EXISTS "meals";
 
-CREATE TABLE "users" (
-                         "user_id" serial NOT NULL,
-                         "name" TEXT NOT NULL,
-                         "email" TEXT NOT NULL UNIQUE,
-                         "password" char(255) NOT NULL,
-                         "enabled" bool NOT NULL DEFAULT 'false',
-                         "registered" TIMESTAMP NOT NULL,
-                         "vote_on_day_now" BOOLEAN NOT NULL DEFAULT 'false',
-                         CONSTRAINT "users_pk" PRIMARY KEY ("user_id")
+ALTER TABLE "history_votes_users"
+    DROP CONSTRAINT IF EXISTS "history_votes_users_fk0";
+
+ALTER TABLE "history_votes_users"
+    DROP CONSTRAINT IF EXISTS "history_votes_users_fk1";
+
+ALTER TABLE "roles"
+    DROP CONSTRAINT IF EXISTS "roles_fk0";
+
+ALTER TABLE "meals"
+    DROP CONSTRAINT IF EXISTS "meals_fk0";
+
+CREATE TABLE "users"
+(
+    "id"         serial    NOT NULL,
+    "name"       TEXT      NOT NULL,
+    "email"      TEXT      NOT NULL UNIQUE,
+    "password"   char(255) NOT NULL,
+    "enabled"    bool      NOT NULL DEFAULT 'false',
+    "registered" TIMESTAMP NOT NULL,
+    CONSTRAINT "users_pk" PRIMARY KEY ("id")
+) WITH ( OIDS= FALSE );
+
+
+
+CREATE TABLE "restaraunts"
+(
+    "restaraunt_id"   serial NOT NULL,
+    "name_restaraunt" TEXT   NOT NULL,
+    "vote_count"      serial NOT NULL,
+    "menu"            TEXT   NOT NULL,
+    CONSTRAINT "restaraunts_pk" PRIMARY KEY ("restaraunt_id")
 ) WITH (
-      OIDS=FALSE
+      OIDS= FALSE
     );
 
 
 
-CREATE TABLE "restaraunt" (
-                              "restaraunt_id" serial NOT NULL,
-                              "name_restaraunt" TEXT NOT NULL,
-                              "vote_count" serial NOT NULL,
-                              "menu" TEXT NOT NULL,
-                              CONSTRAINT "restaraunt_pk" PRIMARY KEY ("restaraunt_id")
+CREATE TABLE "history_votes_users"
+(
+    "history_votes_id" serial    NOT NULL,
+    "user_id"          bigint    NOT NULL,
+    "date_vote"        TIMESTAMP NOT NULL,
+    "restaraunt_id"    bigint    NOT NULL,
+    CONSTRAINT "history_votes_users_pk" PRIMARY KEY ("history_votes_id")
 ) WITH (
-      OIDS=FALSE
+      OIDS= FALSE
+    );
+
+CREATE TABLE "roles"
+(
+    "user_id" bigint       NOT NULL,
+    "role"    varchar(255) NOT NULL
+) WITH (
+      OIDS= FALSE
     );
 
 
 
-CREATE TABLE "history_votes" (
-                                 "history_votes_id" serial NOT NULL,
-                                 "user_name" TEXT NOT NULL,
-                                 "date_vote" TIMESTAMP NOT NULL,
-                                 "history_fk" int NOT NULL,
-                                 "vote_user" int NOT NULL DEFAULT '0',
-                                 CONSTRAINT "history_votes_pk" PRIMARY KEY ("history_votes_id")
+CREATE TABLE "meals"
+(
+    "menu_id"       serial NOT NULL,
+    "description"   TEXT   NOT NULL,
+    "price"         float4 NOT NULL,
+    "restaraunt_id" bigint NOT NULL,
+    CONSTRAINT "meals_pk" PRIMARY KEY ("menu_id")
 ) WITH (
-      OIDS=FALSE
+      OIDS= FALSE
     );
 
 
 
-CREATE TABLE "roles" (
-                         "user_id" bigint NOT NULL,
-                         "role" varchar(255) NOT NULL,
-                         "user_roles_idx" bigint NOT NULL
-) WITH (
-      OIDS=FALSE
-    );
+ALTER TABLE "history_votes_users"
+    ADD CONSTRAINT "history_votes_users_fk0" FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "history_votes_users"
+    ADD CONSTRAINT "history_votes_users_fk1" FOREIGN KEY ("restaraunt_id") REFERENCES "restaraunts" ("restaraunt_id");
+
+ALTER TABLE "roles"
+    ADD CONSTRAINT "roles_fk0" FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "meals"
+    ADD CONSTRAINT "meals_fk0" FOREIGN KEY ("restaraunt_id") REFERENCES "restaraunts" ("restaraunt_id");
 
 
 
-
-
-ALTER TABLE "history_votes" ADD CONSTRAINT "history_votes_fk0" FOREIGN KEY ("history_fk") REFERENCES "users"("user_id");
-
-ALTER TABLE "roles" ADD CONSTRAINT "roles_fk0" FOREIGN KEY ("user_roles_idx") REFERENCES "users"("user_id");
