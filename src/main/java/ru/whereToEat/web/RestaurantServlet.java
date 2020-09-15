b4a9a117ad8a9e9fa54ed2bc3ff83d8c1dbc0bb3
Валентин <moysait1984@yandex.ru>
2020-09-15 21:45:13 +0400

    add restaurants.jsp

--- src/main/java/ru/whereToEat/web/RestaurantServlet.java
+++ src/main/java/ru/whereToEat/web/RestaurantServlet.java
@@ -2,9 +2,40 @@ package ru.whereToEat.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.whereToEat.model.Restaurant;
+import ru.whereToEat.repository.jdbc.JDBCRestaurantRepository;
+import ru.whereToEat.service.RestaurantService;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 public class RestaurantServlet extends HttpServlet {
     protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    private List<Restaurant> restaurants;
+
+    @Override
+    public void init(ServletConfig config) {
+        RestaurantService service = new RestaurantService(new JDBCRestaurantRepository());
+        restaurants = service.getall();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setAttribute("restaurants", restaurants);
+        request.getRequestDispatcher("jsp/restaurants.jsp").forward(request, response);
+        log.debug("redirect to restaurants");
+
+    }
 }
---
+++ src/main/webapp/jsp/restaurants.jsp
@@ -0,0 +1,33 @@
+<%@ page import="ru.whereToEat.util.TimeUtil" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jstl/fmt" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Restaurants</title>
+</head>
+<body>
+<hr>
+<h3><a href="index.html">На главную</a></h3>
+<hr>
+<h2>Restaurants List</h2>
+<table align="center" border="1">
+
+    <tr>
+        <th>restaraunt_Id</th>
+        <th>name_restaraunt</th>
+        <th>vote_count</th>
+    </tr>
+
+    <c:forEach var="restaurant" items="${restaurants}">
+        <tr>
+            <jsp:useBean id="restaurant" type="ru.whereToEat.model.Restaurant"/>
+
+            <td>${restaurant.restaraunt_Id}</td>
+            <td>${restaurant.name}</td>
+            <td>${restaurant.vote_count}</td>
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
