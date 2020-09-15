77a99c71a21858ece5ff25ab0f309397cdf3fb88
Валентин <moysait1984@yandex.ru>
2020-09-15 21:34:39 +0400

    add meals.jsp

--- src/main/java/ru/whereToEat/web/MealServlet.java
+++ src/main/java/ru/whereToEat/web/MealServlet.java
@@ -2,11 +2,45 @@ package ru.whereToEat.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.whereToEat.model.Meal;
+import ru.whereToEat.model.User;
+import ru.whereToEat.repository.jdbc.JDBCMealRepository;
+import ru.whereToEat.repository.jdbc.JDBCUserRepository;
+import ru.whereToEat.service.MealService;
+import ru.whereToEat.service.UserService;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 public class MealServlet extends HttpServlet {
     protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    private List<Meal> meals;
+
+    @Override
+    public void init(ServletConfig config) {
+        MealService service = new MealService(new JDBCMealRepository());
+        meals = service.getAll(100002);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setAttribute("meals", meals);
+        request.getRequestDispatcher("jsp/meals.jsp").forward(request, response);
+        log.debug("redirect to meals");
+
+    }
 
 
 }
---
+++ src/main/webapp/jsp/meals.jsp
@@ -0,0 +1,36 @@
+<%@ page import="ru.whereToEat.util.TimeUtil" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jstl/fmt" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Meals</title>
+</head>
+<body>
+<hr>
+<h3><a href="index.html">На главную</a></h3>
+<hr>
+<h2>Meals List</h2>
+<table align="center" border="1">
+
+    <tr>
+        <th>mealId</th>
+        <th>description</th>
+        <th>price</th>
+        <th>restaurantId</th>
+    </tr>
+
+    <c:forEach var="meal" items="${meals}">
+        <tr>
+            <jsp:useBean id="meal" type="ru.whereToEat.model.Meal"/>
+
+            <td>${meal.id}</td>
+            <td>${meal.description}</td>
+            <td>${meal.price}</td>
+            <td>${meal.restaurant.restaraunt_Id}</td>
+
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
