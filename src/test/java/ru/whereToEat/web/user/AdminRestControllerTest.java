697ee17e4a3b5313f44a5ea0cd45db28e3400fa0
Валентин <moysait1984@yandex.ru>
2021-01-26 20:29:55 +0400

    objectMapper

---
+++ src/test/java/ru/whereToEat/web/user/AdminRestControllerTest.java
@@ -0,0 +1,24 @@
+package ru.whereToEat.web.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.whereToEat.web.AbstractControllerTest;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.whereToEat.UserTestData.ADMIN_ID;
+
+public class AdminRestControllerTest extends AbstractControllerTest {
+    private static final String REST_URL = AdminRestController.REST_URL + '/';
+
+    @Test
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + ADMIN_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                // https://jira.spring.io/browse/SPR-14472
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+}
