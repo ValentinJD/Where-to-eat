03f5667bf319e5b6417c3ef9cacb2761f1318697
Валентин <moysait1984@yandex.ru>
2021-03-20 11:35:04 +0400

    validation Meal

--- src/main/java/ru/whereToEat/web/meal/MealUIController.java
+++ src/main/java/ru/whereToEat/web/meal/MealUIController.java
@@ -2,11 +2,15 @@ package ru.whereToEat.web.meal;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.whereToEat.model.Meal;
 import ru.whereToEat.model.Restaurant;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/profile/meals")
@@ -33,12 +37,22 @@ public class MealUIController extends AbstractMealController {
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void createOrUpdate(Meal meal, @RequestParam Integer restaurantId) {
+    public ResponseEntity<String> createOrUpdate(@Valid Meal meal, BindingResult result, @RequestParam Integer restaurantId) {
+        if (result.hasErrors()) {
+            String errorFieldsMsg = result.getFieldErrors().stream()
+                    .map(fe -> String.format("[%s] %s", fe.getField(), fe.getDefaultMessage()))
+                    .collect(Collectors.joining("<br>"));
+            return ResponseEntity.unprocessableEntity().body(errorFieldsMsg);
+        }
+
+
         Restaurant restaurant = new Restaurant();
         restaurant.setId(restaurantId);
         meal.setRestaurant(restaurant);
         if (meal.isNew()) {
             super.create(meal);
         }
+
+        return ResponseEntity.ok().build();
     }
 }
